7783ab6b1b4231dc907087402c51e2c1ecdb2ad5
aqeel2022 <100577515+aqeel2022@users.noreply.github.com>
2023-05-30 10:44:55 +0200

    sigmoid activation

---
+++ Activation Functions/sigmoid.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# Generate data
+x = np.linspace(-10, 10, 100)
+y = sigmoid(x)
+
+# Plotting
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('sigmoid(x)')
+plt.title('Sigmoid Activation Function')
+plt.grid(True)
+plt.show()
